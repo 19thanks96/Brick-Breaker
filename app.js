da424fdd595e703535acfa53a376ad69d7b8caba
hellgamer <19thanks96@gmail.com>
2023-05-11 17:57:21 +0300

    add prettierc.json

--- app.js
+++ app.js
@@ -1,35 +1,16 @@
-import * as PIXI from './node_modules/pixi.js/dist/pixi.mjs';
+import * as PIXI from './node_modules/pixi.js/dist/pixi.mjs'
 
-import { Application, Assets, Sprite } from './node_modules/pixi.js/dist/pixi.mjs';
+//import { Application, Assets, Sprite } from './node_modules/pixi.js/dist/pixi.mjs';
+const screen = {
+  width: 1000,
+  height: 700,
+}
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
-const app = new Application();
+let app = new PIXI.Application({ width: screen.width, height: screen.height })
+document.body.appendChild(app.view)
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
-document.body.appendChild(app.view);
+setInterval(step, 1000)
 
-// load the texture we need
-const texture = await Assets.load('bunny.png');
-
-// This creates a texture from a 'bunny.png' image
-const bunny = new Sprite(texture);
-
-// Setup the position of the bunny
-bunny.x = app.renderer.width / 2;
-bunny.y = app.renderer.height / 2;
-
-// Rotate around the center
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// Add the bunny to the scene we are building
-app.stage.addChild(bunny);
-
-// Listen for frame updates
-app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
-});
+function step() {
+    
+}
