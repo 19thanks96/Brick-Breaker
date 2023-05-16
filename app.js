f92c2566f5e783a02ddd920cbd9972a499bf54cf
hellgamer <19thanks96@gmail.com>
2023-05-16 15:59:38 +0300

    resolve problems with axis

--- app.js
+++ app.js
@@ -11,18 +11,20 @@ let app = new PIXI.Application({ width: screen.width, height: screen.height })
 document.body.appendChild(app.view)
 
 const container = new Container()
+container.width = screen.width
+container.height = screen.height
 app.stage.addChild(container)
 let wall = [];
 
 const ball = new PIXI.Graphics()
-ball.x = 230
-ball.y = 320
-ball.width = 5
+ball.x = 460
+ball.y = 640
+ball.width = 10
 ball.beginFill(0xff0000)
-ball.drawCircle(ball.x, ball.y, 10)
+ball.drawCircle(0, 0, 10)
 ball.endFill()
+ball.position.set(ball.x, ball.y)
 container.addChild(ball)
-
 generateBlocks()
 generatePad()
 function step() {
@@ -33,12 +35,14 @@ function generateBlocks() {
     for (let row = 0; row < brick.rows; row++) {
         for (let column = 0; column < brick.column; column++) {
             const myBrick = new PIXI.Graphics()
-            myBrick.beginFill(0xffff00)
-            ;(myBrick.x = 50 + row * 50),
-                (myBrick.y = 50 / 2 + column * 50),
-                (myBrick.size = 30)
+            myBrick.beginFill(0xffff00);
+            myBrick.x = 100 + row * 100
+            myBrick.y = 100 / 2 + column * 100
+            myBrick.size = 30
             myBrick.lineStyle(5, 0xff0000)
-            myBrick.drawRect(myBrick.x, myBrick.y, myBrick.size, myBrick.size)
+            myBrick.drawRect(0, 0, myBrick.size, myBrick.size)
+            myBrick.endFill()
+            myBrick.position.set(myBrick.x, myBrick.y)
             container.addChild(myBrick)
             wall.push(myBrick)
         }
@@ -57,14 +61,16 @@ function generatePad() {
 function generateBall() {}
 
 function moveBall() {
-    console.log(ball.x)
+    console.log(ball.y + ball.height)
+    console.log(app.screen.height)
+
     ball.x -= velocity.x
     ball.y -= velocity.y
-    if (ball.x + ball.width > app.screen.width || ball.x < 0) {
+    if (ball.x + ball.width/2 > app.screen.width || ball.x < ball.width/2) {
         velocity.x *= -1; 
         }
-      if (ball.y + ball.height > app.screen.height || ball.y < 0) {
-        velocity.y = -1; 
+      if (ball.y + ball.height/2 > app.screen.height || ball.y < ball.width/2) {
+        velocity.y *= -1; 
       }
     }
 
