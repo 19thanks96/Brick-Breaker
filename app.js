37a2cb21bf8b6eed1eefd5a2750c3d56b0e00ce3
hellgamer <19thanks96@gmail.com>
2023-05-16 21:52:18 +0300

    finish mechanik game

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import * as PIXI from './node_modules/pixi.js/dist/pixi.mjs'
 import { Container } from './node_modules/pixi.js/dist/pixi.mjs'
-import { brick,  velocity } from './gameElement.js'
+import { brick, velocity } from './gameElement.js'
 import { addKeybordMovement, movePad } from './keyboardPad.js'
 import { player } from './pad.js'
 
@@ -17,11 +17,11 @@ container.width = screen.width
 container.height = screen.height
 app.stage.addChild(container)
 let wall = []
-
 const ball = new PIXI.Graphics()
 ball.x = 460
 ball.y = 640
 ball.width = 10
+ball.height = 10
 ball.beginFill(0xff0000)
 ball.drawCircle(0, 0, 10)
 ball.endFill()
@@ -29,7 +29,6 @@ ball.position.set(ball.x, ball.y)
 container.addChild(ball)
 container.addChild(player)
 generateBlocks()
-
 
 addKeybordMovement(app)
 function step() {
@@ -67,34 +66,39 @@ function moveBall() {
     ) {
         velocity.y *= -1
     }
-    if (ball.x + ball.width/2 > player.x && 
+    if (
+        ball.x + ball.width / 2 > player.x &&
         ball.x < player.x + player.width &&
-        ball.y + ball.height/2 > player.y &&
-        ball.y < player.y + player.height/2
+        ball.y + ball.height / 2 > player.y &&
+        ball.y < player.y + player.height / 2
     ) {
-        if(velocity.x < 0 && velocity.y > 0 ) {
+        if (velocity.x < 0 && velocity.y > 0) {
             velocity.y *= -1
         }
-        if(velocity.x > 0 && velocity.y > 0) {
+        if (velocity.x > 0 && velocity.y > 0) {
             velocity.y *= -1
         }
     }
-    wall.forEach((bricks, i) => {
-        if(
-            bricks.x + 30 > ball.x - ball.width/2&&
-            bricks.x - 0 < ball.x + ball.width/2 &&
-            bricks.y + 30 > ball.y - ball.height/2 &&
-            bricks.y - 0 < ball.y + ball.height/2
+    let newWall = wall.filter((bricks) => {
+        if (
+            bricks.x + 30 > ball.x - ball.width / 2 &&
+            bricks.x - 0 < ball.x + ball.width / 2 &&
+            bricks.y + 30 > ball.y - ball.height / 2 &&
+            bricks.y - 0 < ball.y + ball.height / 2
         ) {
-            if (bricks.x+5 < ball.x && ball.x > bricks.x-5) {
+            if (bricks.x  < ball.x && ball.x  > bricks.x + 30 ) {
                 velocity.x *= -1
-            } 
-            if (bricks.y+5 < ball.y && ball.y > brick.x-5) {
-                velocity.y *= -1
+                container.removeChild(bricks)
+                return false // Виключаємо елемент з масиву
             }
-                //container.removeChild(bricks)
-            
+            if (bricks.y  < ball.y && ball.y  > bricks.y + 30 ) {
+                velocity.y *= -1
+                container.removeChild(bricks)
+                return false
+            }
         }
+        return true // Залишаємо елемент у масиві
     })
+    wall = newWall
 }
 app.ticker.add(step)
