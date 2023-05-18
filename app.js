b2eb5af91a71b34a08c37183aac7b7825cd2999b
hellgamer <19thanks96@gmail.com>
2023-05-18 14:56:19 +0300

    split moveBall func to multiple func

--- app.js
+++ app.js
@@ -1,85 +1,68 @@
-import * as PIXI from './node_modules/pixi.js/dist/pixi.mjs'
-import { Container } from './node_modules/pixi.js/dist/pixi.mjs'
-import { screen, brick, velocity } from './gameElement.js'
-import { ball, text, endGameTextStyle,  heart } from './gameObj.js'
-import { addKeybordMovement, movePad } from './keyboardPad.js'
-import { player } from './pad.js'
+import * as PIXI from './node_modules/pixi.js/dist/pixi.mjs';
+import { Container } from './node_modules/pixi.js/dist/pixi.mjs';
+import { screen, brick, velocity } from './gameElement.js';
+import { ball, text, endGameTextStyle, heart } from './gameObj.js';
+import { addKeybordMovement, movePad } from './keyboardPad.js';
+import { player } from './pad.js';
 
-let app = new PIXI.Application({ width: screen.width, height: screen.height })
-document.body.appendChild(app.view)
+let app = new PIXI.Application({ width: screen.width, height: screen.height });
+document.body.appendChild(app.view);
 
-const container = new Container()
-container.width = screen.width
-container.height = screen.height
-app.stage.addChild(container)
+const container = new Container();
+container.width = screen.width;
+container.height = screen.height;
+app.stage.addChild(container);
 
-let elapsed = 0.0
-let sec = 0
-let wall = []
-container.addChild(ball)
+let elapsed = 0.0;
+let sec = 0;
+let wall = [];
+container.addChild(ball);
 
-container.addChild(player)
+container.addChild(player);
 
-generateBlocks()
+generateBlocks();
 
-container.addChild(text)
+container.addChild(text);
 
 let velocityCollisionPad;
 
-container.addChild(heart)
+container.addChild(heart);
 
-addKeybordMovement(app)
+addKeybordMovement(app);
 
-let lives = 5
+let lives = 5;
 
-const life = new PIXI.Text(lives, endGameTextStyle)
-life.x = screen.width - 20 - 10
-life.y = 0 + life.width
-container.addChild(life)
+const life = new PIXI.Text(lives, endGameTextStyle);
+life.x = screen.width - 20 - 10;
+life.y = 0 + life.width;
+container.addChild(life);
 
 function step(delta) {
-    elapsed += delta
-    movePad()
-    moveBall()
-    finish(elapsed)
-    life.text = lives
+    elapsed += delta;
+    movePad();
+    moveBall();
+    finish(elapsed);
+    life.text = lives;
 }
 
 function generateBlocks() {
     for (let row = 0; row < brick.rows; row++) {
         for (let column = 0; column < brick.column; column++) {
-            const myBrick = new PIXI.Graphics()
-            myBrick.beginFill(0xffff00)
-            myBrick.x = 100 + row * 100
-            myBrick.y = 100 / 2 + column * 100
-            myBrick.size = 30
+            const myBrick = new PIXI.Graphics();
+            myBrick.beginFill(0xffff00);
+            myBrick.x = 100 + row * 100;
+            myBrick.y = 100 / 2 + column * 100;
+            myBrick.size = 30;
             //myBrick.lineStyle(5, 0xff0000)
-            myBrick.drawRect(0, 0, myBrick.size, myBrick.size)
-            myBrick.endFill()
-            myBrick.position.set(myBrick.x, myBrick.y)
-            container.addChild(myBrick)
-            wall.push(myBrick)
+            myBrick.drawRect(0, 0, myBrick.size, myBrick.size);
+            myBrick.endFill();
+            myBrick.position.set(myBrick.x, myBrick.y);
+            container.addChild(myBrick);
+            wall.push(myBrick);
         }
     }
 }
-
-function moveBall() {
-    ball.x += velocity.x
-    ball.y += velocity.y
-    if (ball.x + ball.width / 2 > app.screen.width || ball.x < ball.width / 2) {
-        velocity.x *= -1
-    }
-    if (
-        ball.y + ball.height / 2 > app.screen.height ||
-        ball.y < ball.width / 2
-    ) {
-        velocity.y *= -1
-    }
-
-    if (ball.y >= 690) {
-        lives -= 1
-    }
-
+function collisionPlatformAndBall() {
     if (
         ball.x + ball.width / 2 > player.x &&
         ball.x < player.x + player.width &&
@@ -88,56 +71,82 @@ function moveBall() {
     ) {
         if (velocity.x < 0 && velocity.y > 0) {
             velocityCollisionPad =
-                (player.x - ball.x + player.width / 2 + 2) * 0.05 * -1
-            velocity.x += velocityCollisionPad
-            velocity.y *= -1
+                (player.x - ball.x + player.width / 2 + 2) * 0.05 * -1;
+            velocity.x += velocityCollisionPad;
+            velocity.y *= -1;
         }
         if (velocity.x > 0 && velocity.y > 0) {
             velocityCollisionPad =
-                (player.x - ball.x + player.width / 2 + 2) * 0.05 * -1
-            velocity.x += velocityCollisionPad
-            velocity.y *= -1
+                (player.x - ball.x + player.width / 2 + 2) * 0.05 * -1;
+            velocity.x += velocityCollisionPad;
+            velocity.y *= -1;
         }
     }
-    let newWall = wall.filter((bricks) => {
-        if (
-            bricks.x + bricks.width > ball.x - ball.width / 2 &&
-            bricks.x < ball.x + ball.width / 2 &&
-            bricks.y + bricks.height > ball.y - ball.height / 2 &&
-            bricks.y < ball.y + ball.height / 2
-        ) {
-            if (bricks.x < ball.x && ball.x < bricks.x + bricks.width) {
-                velocity.y *= -1
-                container.removeChild(bricks)
-                return false
-            }
-            if (bricks.y < ball.y && ball.y < bricks.y + bricks.width) {
-                velocity.x *= -1
-                container.removeChild(bricks)
-                return false
-            } 
-            velocity.y *= -1
-            velocity.x *= -1
-                container.removeChild(bricks)
-                return false
+}
+function ballFell() {
+    if (ball.y >= 690) {
+        lives -= 1;
+    }
+}
+function collisionScreenAndBall() {
+    if (ball.x + ball.width / 2 > app.screen.width || ball.x < ball.width / 2) {
+        velocity.x *= -1;
+    }
+    if (
+        ball.y + ball.height / 2 > app.screen.height ||
+        ball.y < ball.width / 2
+    ) {
+        velocity.y *= -1;
+    }
+}
+function collisionBrickAndBall(bricks) {
+    if (
+        bricks.x + bricks.width > ball.x - ball.width / 2 &&
+        bricks.x < ball.x + ball.width / 2 &&
+        bricks.y + bricks.height > ball.y - ball.height / 2 &&
+        bricks.y < ball.y + ball.height / 2
+    ) {
+        if (bricks.x < ball.x && ball.x < bricks.x + bricks.width) {
+            velocity.y *= -1;
+            container.removeChild(bricks);
+            return false;
         }
-        return true
-    })
-    wall = newWall
+        if (bricks.y < ball.y && ball.y < bricks.y + bricks.width) {
+            velocity.x *= -1;
+            container.removeChild(bricks);
+            return false;
+        }
+        velocity.y *= -1;
+        velocity.x *= -1;
+        container.removeChild(bricks);
+        return false;
+    }
+    return true;
 }
 
+function moveBall() {
+    ball.x += velocity.x;
+    ball.y += velocity.y;
+
+    collisionScreenAndBall();
+    collisionPlatformAndBall();
+    ballFell();
+
+    let newWall = wall.filter(collisionBrickAndBall);
+    wall = newWall;
+}
 
 function finish(elapsed) {
-    sec = Math.floor(elapsed / 60)
+    sec = Math.floor(elapsed / 60);
 
     if (wall.length === 0) {
-        text.y = screen.height / 2
-        ;text.text = `You Win after ${sec} second and have ${lives} lives`
-        text.x = screen.width / 2 - text.width/2
-        app.ticker.stop()
+        text.y = screen.height / 2;
+        text.text = `You Win after ${sec} second and have ${lives} lives`;
+        text.x = screen.width / 2 - text.width / 2;
+        app.ticker.stop();
     } else {
-        text.text = sec
+        text.text = sec;
     }
 }
 
-app.ticker.add(step)
+app.ticker.add(step);
